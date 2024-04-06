3b7d71030897d657aa981589db9190bc90dd8426
elijahmstarline <35530811+elijah999mgenezis@users.noreply.github.com>
2024-04-06 10:45:08 +0200

    clean

--- src/components/icons.js
+++
@@ -1,8 +0,0 @@
-export { ReactComponent as DigitaloceanIcon } from './../icons/digitalocean.svg';
-import { ReactComponent as kubernetesIcon } from './../icons/kubernetes.svg';
-
-export const Kubernetes = () => (
-  <div className="text-red-500"> {/* Use Tailwind CSS classes for styling */}
-    <img src={KubernetesIcon} alt="Kubernetes Icon" className="w-8 h-8" />
-  </div>
-);
